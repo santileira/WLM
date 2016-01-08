99a97b5801b7929c0ba4bd20b62ce3c6e5f9322d
braianhal <braianhal@gmail.com>
2016-01-08 17:44:52 -0300

    mail

--- web/js/send_email.php
+++ web/js/send_email.php
@@ -1,13 +1,14 @@
 <?php
 if (isset($_REQUEST['email']))  {
     //Email information
-    $admin_email = "webdevelopment@atis.al";
-    $name = $_REQUEST['first_name'];
-    $email = $_REQUEST['email'];
-    $message = $_REQUEST['message'];
+    $admin_email = "santiagoleira10@gmail.com";
+    $name = $_REQUEST['Nombre'];
+    $email = $_REQUEST['E-mail'];
+	$telefono = $_REQUEST['Telefono'];
+    $message = $_REQUEST['Mensaje'];
 
     //send email
-    if (mail($admin_email, $name, $message, "From:" . $email)) {
+    if (mail($admin_email, $name, $email, $message, "From:" . $email)) {
         echo 1;
     }
     else {
