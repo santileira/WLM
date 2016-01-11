a18bcb42e865d0e5c481f1c17d48706e7e9722f6
santileira <santiagoleira10@gmail.com>
2016-01-11 17:21:40 -0300

    cambios mail

--- web/js/send_email.php
+++ web/js/send_email.php
@@ -6,9 +6,16 @@ if (isset($_REQUEST['E-mail']))  {
     $email = $_REQUEST['E-mail'];
 	$telefono = $_REQUEST['Telefono'];
     $message = $_REQUEST['Mensaje'];
+	$asunto = "E-mail Contacto - CABAÑAS LA MORENA";
+	
+	$mensajeFinal = "Nombre: " . $name . " \r\n"; 
+	$mensajeFinal .= "E-mail: " . $email . " \r\n"; 
+	$mensajeFinal .= "Teléfono: " . $telefono . " \r\n";
+	$mensajeFinal .= "Mensaje: " . $message .  " \r\n"; 
+
 
     //send email
-    if (mail($admin_email, $name, $email, $message, "From:" . $email)) {
+    if (mail($admin_email, $asunto , $mensajeFinal, "From:" . $email)) {
         echo 1;
     }
     else {
